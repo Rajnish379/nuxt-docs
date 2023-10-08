02b464000d4b2b3dbb3bad3504f0f5440bc7dc3e
Rajnish379 <rajinish77@gmail.com>
2023-10-08 12:50:58 -0500

    Final commit for nux docs

---
+++ server/api/countries/find.post.ts
@@ -0,0 +1,15 @@
+// api/countries/find
+// method post
+// post {country: string} 'France'
+
+import {countryList} from "@doubco/countries";
+
+console.log(countryList);
+
+export default eventHandler(async (event) => {
+    const body = await readBody(event); // body {}
+    const {country} = body;
+    return {
+        countries: countryList.filter((obj) => obj.label.includes(country)),
+    };
+})
---
+++ server/api/random.get.ts
@@ -0,0 +1,7 @@
+import { countryList } from "@doubco/countries";
+console.log(countryList)
+export default () => {
+    return {
+        random_country: countryList[Math.floor(Math.random()*countryList.length)]
+    };
+};
